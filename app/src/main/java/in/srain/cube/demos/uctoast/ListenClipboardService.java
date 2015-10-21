d1f62cd4cfa2fc8c8d87cd5d3e4ee7ed5e1812b3
liaohuqiu <liaohuqiu@gmail.com>
2015-10-21 14:48:46 -0700

    try to fix #2

--- app/src/main/java/in/srain/cube/demos/uctoast/ListenClipboardService.java
+++ app/src/main/java/in/srain/cube/demos/uctoast/ListenClipboardService.java
@@ -3,17 +3,12 @@ package in.srain.cube.demos.uctoast;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.PixelFormat;
-import android.graphics.Rect;
-import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.text.TextUtils;
-import android.view.*;
-import android.widget.TextView;
 import in.srain.cube.demos.uctoast.clipboard.ClipboardManagerCompat;
 
-public final class ListenClipboardService extends Service {
+public final class ListenClipboardService extends Service implements TipViewController.ViewDismissHandler {
 
     private static final String KEY_FOR_WEAK_LOCK = "weak-lock";
     private static final String KEY_FOR_CMD = "cmd";
@@ -22,7 +17,7 @@ public final class ListenClipboardService extends Service {
 
     private static CharSequence sLastContent = null;
     private ClipboardManagerCompat mClipboardWatcher;
-
+    private TipViewController mTipViewController;
     private ClipboardManagerCompat.OnPrimaryClipChangedListener mOnPrimaryClipChangedListener = new ClipboardManagerCompat.OnPrimaryClipChangedListener() {
         public void onPrimaryClipChanged() {
             performClipboardCheck();
@@ -67,6 +62,12 @@ public final class ListenClipboardService extends Service {
     public void onDestroy() {
         super.onDestroy();
         mClipboardWatcher.removePrimaryClipChangedListener(mOnPrimaryClipChangedListener);
+
+        sLastContent = null;
+        if (mTipViewController != null) {
+            mTipViewController.setViewDismissHandler(null);
+            mTipViewController = null;
+        }
     }
 
     @Override
@@ -109,101 +110,18 @@ public final class ListenClipboardService extends Service {
         }
         sLastContent = content;
 
-        TipViewController controller = new TipViewController(getApplication(), content);
-        controller.show();
+        if (mTipViewController != null) {
+            mTipViewController.updateContent(content);
+        } else {
+            mTipViewController = new TipViewController(getApplication(), content);
+            mTipViewController.setViewDismissHandler(this);
+            mTipViewController.show();
+        }
     }
 
-    final static class TipViewController implements View.OnClickListener, View.OnTouchListener, ViewContainer.KeyEventHandler {
-
-        private WindowManager mWindowManager;
-        private Context mContext;
-        private ViewContainer mWholeView;
-        private View mContentView;
-
-        private CharSequence mContent;
-
-        TipViewController(Context application, CharSequence content) {
-            mContext = application;
-            mContent = content;
-            mWindowManager = (WindowManager) application.getSystemService(Context.WINDOW_SERVICE);
-        }
-
-        private void show() {
-
-            ViewContainer view = (ViewContainer) View.inflate(mContext, R.layout.pop_view, null);
-
-            // display content
-            TextView textView = (TextView) view.findViewById(R.id.pop_view_text);
-            textView.setText(mContent);
-
-            mWholeView = view;
-            mContentView = view.findViewById(R.id.pop_view_content_view);
-
-            // event listeners
-            mContentView.setOnClickListener(this);
-            mWholeView.setOnTouchListener(this);
-            mWholeView.setKeyEventHandler(this);
-
-            int w = WindowManager.LayoutParams.MATCH_PARENT;
-            int h = WindowManager.LayoutParams.MATCH_PARENT;
-
-            int flags = 0;
-            int type = 0;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                type = WindowManager.LayoutParams.TYPE_TOAST;
-            } else {
-                type = WindowManager.LayoutParams.TYPE_PHONE;
-            }
-
-            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(w, h, type, flags, PixelFormat.TRANSLUCENT);
-            layoutParams.gravity = Gravity.TOP;
-
-            mWindowManager.addView(mWholeView, layoutParams);
-        }
-
-        @Override
-        public void onClick(View v) {
-            removePoppedViewAndClear();
-            MainActivity.startForContent(mContext, mContent.toString());
-        }
-
-        private void removePoppedViewAndClear() {
-
-            // remove view
-            if (mWindowManager != null && mWholeView != null) {
-                mWindowManager.removeView(mWholeView);
-            }
-
-            // clear content
-            sLastContent = null;
-
-            // remove listeners
-            mContentView.setOnClickListener(null);
-            mWholeView.setOnTouchListener(null);
-            mWholeView.setKeyEventHandler(null);
-        }
-
-        /**
-         * touch the outside of the content view, remove the popped view
-         */
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            int x = (int) event.getX();
-            int y = (int) event.getY();
-            Rect rect = new Rect();
-            mContentView.getGlobalVisibleRect(rect);
-            if (!rect.contains(x, y)) {
-                removePoppedViewAndClear();
-            }
-            return false;
-        }
-
-        @Override
-        public void onKeyEvent(KeyEvent event) {
-            int keyCode = event.getKeyCode();
-            if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
-                removePoppedViewAndClear();
-            }
-        }
+    @Override
+    public void onViewDismiss() {
+        sLastContent = null;
+        mTipViewController = null;
     }
 }
--- app/src/main/java/in/srain/cube/demos/uctoast/MainActivity.java
+++ app/src/main/java/in/srain/cube/demos/uctoast/MainActivity.java
@@ -2,13 +2,15 @@ package in.srain.cube.demos.uctoast;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
 import android.widget.TextView;
 
 public final class MainActivity extends AppCompatActivity {
